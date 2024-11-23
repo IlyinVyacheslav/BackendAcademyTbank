76b395380f36cf04cd58b3f9cb624534d451a19c
Ilin_Viacheslav <367255@niuitmo.ru>
2024-11-23 21:53:18 +0300

    fixing adding hw3

--- backend_academy_2024_project_3-java-IlyinVyacheslav/src/main/java/backend/academy/parser/IncorrectLogException.java
+++
@@ -1,7 +0,0 @@
-package backend.academy.parser;
-
-public class IncorrectLogException extends IllegalArgumentException {
-    public IncorrectLogException(String message) {
-        super(message);
-    }
-}
--- backend_academy_2024_project_3-java-IlyinVyacheslav/src/main/java/backend/academy/statistics/writer/StatisticsWriter.java
+++
@@ -1,7 +0,0 @@
-package backend.academy.statistics.writer;
-
-import backend.academy.statistics.Statistics;
-
-public interface StatisticsWriter {
-    String writeStatistics(Statistics statistics);
-}
