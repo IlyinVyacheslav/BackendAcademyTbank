4a73158c3dd09da6ac54bbc95fd1a16c921cb5bb
Ilin_Viacheslav <367255@niuitmo.ru>
2025-03-02 16:40:19 +0300

    logger logic changed from mdc to at..

--- logger/src/main/java/backend/academy/logger/LoggerHelper.java
+++ logger/src/main/java/backend/academy/logger/LoggerHelper.java
@@ -6,16 +6,15 @@ import java.util.Map;
 import lombok.experimental.UtilityClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
+import org.slf4j.spi.LoggingEventBuilder;
 
 @UtilityClass
 public class LoggerHelper {
     private static final ObjectMapper objectMapper = new ObjectMapper();
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggerHelper.class);
-    private static final String FORMAT = "Message: {}, Context: {}";
 
     public void info(String message, Map<String, Object> context) {
-        withMDC(context, () -> LOGGER.info(FORMAT, message, toJson(context)));
+        logWithContext(LOGGER.atInfo(), message, context);
     }
 
     public void info(String message) {
@@ -23,7 +22,7 @@ public class LoggerHelper {
     }
 
     public void error(String message, Map<String, Object> context, Throwable error) {
-        withMDC(context, () -> LOGGER.error(FORMAT, message, toJson(context), error));
+        logWithContext(LOGGER.atError().setCause(error), message, context);
     }
 
     public void error(String message, Throwable error) {
@@ -31,22 +30,11 @@ public class LoggerHelper {
     }
 
     public void warn(String message, Map<String, Object> context) {
-        withMDC(context, () -> LOGGER.warn(FORMAT, message, toJson(context)));
+        logWithContext(LOGGER.atWarn(), message, context);
     }
 
     public void debug(String message, Map<String, Object> context) {
-        withMDC(context, () -> LOGGER.debug(FORMAT, message, toJson(context)));
-    }
-
-    private void withMDC(Map<String, Object> context, Runnable action) {
-        try {
-            context.forEach((key, value) -> {
-                MDC.put(key, toJson(value));
-            });
-            action.run();
-        } finally {
-            MDC.clear();
-        }
+        logWithContext(LOGGER.atDebug(), message, context);
     }
 
     private static String toJson(Object object) {
@@ -55,5 +43,15 @@ public class LoggerHelper {
         } catch (JsonProcessingException e) {
             return null;
         }
+    }
+
+    private void logWithContext(LoggingEventBuilder loggingEventBuilder, String message, Map<String, Object> context) {
+        loggingEventBuilder = loggingEventBuilder.setMessage(message);
+        for (Map.Entry<String, Object> entry : context.entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            loggingEventBuilder = loggingEventBuilder.addKeyValue(key, toJson(value));
+        }
+        loggingEventBuilder.log();
     }
 }
